218049f0edd93c89a2e1f0b77d26f5ae253237bc
GigaHierz <lenahierzi@hotmail.de>
2025-11-17 10:40:55 -0300

    rename BuenaToken to BuenoToken

--- contracts/BuenoToken.sol
+++ contracts/BuenoToken.sol
@@ -5,11 +5,11 @@ import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts@5.4.0/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
 
-contract BuenaToken is ERC20, Ownable, ERC20Permit {
+contract Buen0Token is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner)
-        ERC20("BuenaToken", "BTK")
+        ERC20("BuenoToken", "BTK")
         Ownable(initialOwner)
-        ERC20Permit("BuenaToken")
+        ERC20Permit("BuenoToken")
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
